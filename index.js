15cf126a4e105472f95f47847a2aaccf5f47e77f
fady <fady.khairy164@gmail.com>
2023-12-31 13:52:16 +0200

    commit

---
+++ index.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const cors = require("cors");
+const KJUR = require("jsrsasign");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json(), cors());
+app.options("*", cors());
+
+app.post("/post", (req, res) => {
+  const iat = Math.round(new Date().getTime() / 1000) - 30;
+  const exp = iat + 60 * 60 * 2;
+
+  console.log(iat, "iat");
+  console.log(exp, "exp");
+
+  const oHeader = { alg: "HS256", typ: "JWT" };
+
+  console.log(oHeader, "oHeader");
+
+  const oPayload = {
+    appKey: process.env.ZOOM_MEETING_SDK_KEY,
+    sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
+    iat: iat,
+    exp: exp,
+    role: 0,
+    mn: req.body.meetingNumber,
+  };
+  console.log(oPayload, "oPayload");
+
+  const sHeader = JSON.stringify(oHeader);
+  const sPayload = JSON.stringify(oPayload);
+
+  console.log(sHeader, "sHeader");
+  console.log(sPayload, "sPayload");
+
+  const signature = KJUR.jws.JWS.sign(
+    "HS256",
+    sHeader,
+    sPayload,
+    process.env.ZOOM_MEETING_SDK_SECRET
+  );
+
+  res.json({
+    signature: signature,
+  });
+});
+
+app.listen(port, () =>
+  console.log(
+    `Zoom Meeting SDK Auth Endpoint Sample Node.js listening on port ${port}!`
+  )
+);
